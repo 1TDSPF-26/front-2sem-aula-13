d145207e5ced42ff2e79943d9e92fbf718122a80
gabriel <gabrielrazod9j@gmail.com>
2026-09-16 16:09:19 -0300

    feat: Adicionando comentarios no Home da api.

--- my-app/src/routes/Home/index.tsx
+++ my-app/src/routes/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+// Tipagem TypeScript definindo o formato dos dados do usuário que virão da API
 type TipoUsuarioGit = {
   login: string;
   id: number;
@@ -25,22 +25,22 @@ type TipoUsuarioGit = {
 export default function Home() {
   // Para alterar o título da página:
   document.title = "Home";
-
+  // Cria a memória do componente para guardar a lista de usuários.(altera o estado), Começa como um array vazio ([]).
   const [usuarios, setUsuarios] = useState<TipoUsuarioGit[]>([]);
 
-  useEffect( ()=>{
+  useEffect( ()=>{ //Executa o código dentro dele assim que o componente é montado.
 
-    async function loadingData(){
+    async function loadingData(){ // Função assíncrona para buscar os dados na API do GitHub
         
         try {
-            const response = await fetch("https://api.github.com/users");
+            const response = await fetch("https://api.github.com/users"); // Inicia a solicitação assíncrona à API
             
-            if(!response.ok){
+            if(!response.ok){ // Valida se o código de status HTTP está entre 200 e 299 (sucesso)
                 throw new Error("A listagem de usuários falhou!");
             }
 
-            const data:TipoUsuarioGit[] = await response.json();
-            setUsuarios(data);
+            const data:TipoUsuarioGit[] = await response.json(); // Converte o corpo da resposta de JSON para um array JavaScript
+            setUsuarios(data); // Guarda os dados obtidos na memória do componente, o que faz a tela atualizar
 
        } catch (error) {
         console.error(error);
@@ -48,7 +48,7 @@ export default function Home() {
        
     }
 
-    loadingData();
+    loadingData(); // Chama a função de busca
 
   },[] );
 
@@ -57,7 +57,8 @@ export default function Home() {
       <h2>Home</h2>
       <div>
         <ul>
-            {usuarios.map((u,i)=>(
+            {usuarios.map((u,i)=>( //O map percorre a lista e cria um elemento HTML para cada usuário
+            // A prop 'key' é essencial para o React identificar de forma única cada item da lista
                     <li key={i}>{u.id} - {u.login} - <img src={u.avatar_url} alt={u.login} width={30}/></li>
             ))}
         </ul>
