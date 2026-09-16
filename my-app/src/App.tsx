066e77ad3ede7a94412d19e405ad667b01fff75a
gabriel <gabrielrazod9j@gmail.com>
2026-09-16 11:49:06 -0300

    feat: Consumindo api externa com HTTP-GET.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router'
 import Cabecalho from './components/Cabecalho/Cabecalho'
-import Conteudo from './components/Conteudo/Conteudo'
-import Lampada from './components/Lampada'
+//import Conteudo from './components/Conteudo/Conteudo'
+//import Lampada from './components/Lampada'
 import Rodape from './components/Rodape/Rodape'
 
 export default function App() {
@@ -9,7 +9,7 @@ export default function App() {
     <div>
       <Cabecalho />
       <Outlet/>
-      <Lampada />
+      {/* <Lampada /> */}
       <Rodape />
     </div>
   )
